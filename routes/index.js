696415d54efe0262a7267d8920b63b10965e46ea
James McKay <james.mckay215@yahoo.com>
2022-01-09 21:14:46 -0800

    Added android download button to main page

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,19 @@
 var express = require('express')
+const got = require('got')
 var router = express.Router()
 
 /* GET home page. */
-router.get('/', function(req, res, next) {
-    res.render('index')
+router.get('/', async function(req, res, next) {
+    const response          = await got('https://api.github.com/repos/Schoolboy215/boggle-client/releases/latest')
+    const parsedResponse     = JSON.parse(response.body)
+    if (parsedResponse["assets"][0]["browser_download_url"])
+    {
+        res.render('index',{latest_download_url:parsedResponse["assets"][0]["browser_download_url"]})
+    }
+    else
+    {
+        res.render('index')
+    }
 })
 
 module.exports = router
